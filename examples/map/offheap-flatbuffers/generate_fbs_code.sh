0733fb9cd86257da6c2184df283a64fa4af53dd2
Aaron Riekenberg <aaron.riekenberg@target.com>
2022-06-09 14:11:50 -0500

    Add comment.

--- examples/map/offheap-flatbuffers/generate_fbs_code.sh
+++ examples/map/offheap-flatbuffers/generate_fbs_code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Script to invoke the FlatBuffers compiler flatc to generate Kotlin FlatBuffer classes.
+# You must have flatc installed locally and in your PATH before running this script.
 
 BASE_DIRECTORY=$(dirname $0)
 
