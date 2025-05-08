4dcb2724735b16b9b9a605f199cbfe7a11ece767
Aaron.Riekenberg <aaron.riekenberg@target.com>
2025-05-08 10:36:06 -0500

    OffHeapEviction example works.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ include(
     "examples:map:utils",
     "examples:map:onheap",
     "examples:map:offheap",
-//    "examples:map:offheap-eviction",
+    "examples:map:offheap-eviction",
 //    "examples:map:offheap-eviction-operationcounters",
 //    "examples:map:offheap-flatbuffers",
 )
